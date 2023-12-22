b49d0853fab3e7463a0a404211e0863de2b933dd
Quanbui86 <136612066+Quanbui86@users.noreply.github.com>
2023-12-22 15:47:59 +0700

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -3,11 +3,37 @@ import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const token = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkODhlZmU3YTIyZmM1NWJlM2IyOTg3ZjE1ZWNmNDFlNCIsInN1YiI6IjY1NGYyNjQ0MjkzODM1NDNmNDg2MDkyNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FWHK4DfBohjiYyiqykkvprCiNY1BvLDR2mbePlzhzII'
+
   // Element selectors
 const [json, setJson] = useState('')
 const [jsObject, setJsObject] = useState('')
-
+const [movies, setMovies] = useState('')
 // Text to populate button text on click
+const getFilm = () => {
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${token}`
+    }
+  };
+  fetch(`https://api.themoviedb.org/3/search/movie?query=hero&include_adult=true&language=en-US&page=1`, options)
+    .then(response => response.json())
+    .then(response => {
+      console.log(response)
+      getrandomMovieInfo(response)
+    })
+    .catch(err => console.error(err));
+}
+function getrandomMovieInfo(response){
+  const random = Math.floor(Math.random()*response.results.length)
+  console.log(response.results[random])
+  setMovies(JSON.stringify(response.results[random], null, 2))
+}
+const clickHandleFilm = () => {
+  getFilm()
+}
 
 // Asynchronous function
 const generateJson = async () => {
@@ -15,9 +41,6 @@ const generateJson = async () => {
     const response = await fetch('https://jsonplaceholder.typicode.com/users');
     if(response.ok){
       const jsonResponse = await response.json();
-      // Kiểm tra định dạng của jsonResponse, là 1 array có 10 phần tử là object
-      
-      // Gán json là giá trị của jsonResponse dưới định dạng JSON 
       setJsObject(jsonResponse)
       randomObject(jsonResponse)
     }
@@ -47,10 +70,11 @@ useEffect(() => {
     <div className="App">
       <header className="App-header">
         <section className='container'>
-          {json?<pre className='textArear' >{json}</pre>:''}
           <input className='button' type='button' value='Generate' onClick={clickHandle}/>
+          {json?<pre className='textArear' >{json}</pre>:''}
+          <input className='button' type='button' value='Generate Film' onClick={clickHandleFilm}/>
+          <pre className='display'>{movies}</pre>
         </section>
-        
       </header>
     </div>
   );
